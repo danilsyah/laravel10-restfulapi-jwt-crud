35a47e0fe927e8ed7019a5fffc185cdbfd737691
Danilsyah <danilsyaharihardjo@gmail.com>
2023-10-27 06:21:05 +0700

    init project

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User; // memanggil model
+use Illuminate\Support\Facades\Auth; // memanggil class Auth
+use Illuminate\Support\Facades\Hash; // memanggil class Hash untuk enkrip
+use Illuminate\Support\Facades\Validator; // memanggil class validator
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        // validasi request
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|string|between:2,100',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|confirmed|min:6',
+        ]);
+
+        // jika ada validasi yang error kita akan berikan pesan error
+        if($validator->fails()){
+            return response->json($validator->errors()->toJson(), 400);
+        }
+
+        // mengambil array data request yang sudah validated
+        $validatedData = $validator->validated();
+
+        // enkrip password dengan hash
+        $validatedData['password'] = Hash::make($request['password']);
+
+        // Insert user ke table users
+        $user = User::create($validatedData);
+
+        // create token
+        $token = Auth::attempt($request->only(['email', 'password']));
+
+        return $this->createNewToken($token);
+    }
+
+    public function login(Request $request){
+
+    }
+
+    public function refresh(){
+
+    }
+
+    public function me(){
+
+    }
+
+    protected function createNewToken($token){
+        return response()->json([
+            'access_token'=>$token,
+            'token_type'=>'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()
+        ]);
+
+    }
+}
---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+// JWT
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+
+class JwtMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        try {
+            $user = JWTAuth::parseToken()->authentication();
+        } catch (\Exception $e) {
+            if($e instanceof TokenInvalidException){
+                return response()->json(['status'=>'Token is Invalid']);
+            }else if($e instanceof TokenExpiredException){
+                return response()->json(['status' => 'Token is Expired']);
+            }
+            return response()->json(['status'=>'Auhorization Token not found']);
+        }
+        return $next($request);
+    }
+}
